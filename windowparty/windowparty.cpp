1a547cfd52d936b60d804714558213791064dc1c
kwakJakyeong <dev.ripetangerine@gmail.com>
2026-04-01 09:09:41 +0900

    feat: ms new window course

--- windowparty/windowparty.cpp
+++ windowparty/windowparty.cpp
@@ -1,14 +1,31 @@
 ﻿
 // windowparty.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
+#ifndef UNICODE
+#define UNICODE
+#endif
 
 #include <iostream>
 #include <windows.h>
+#include <WinUser.h>
 
 // 무한 < esc로 종료
 // 2초에 한번씩 마우스를 따라다니면서 윈도우 창이뜸
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+//LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+//    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+//}
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wRaram, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdshow) 
+{
+    // register window class
+    const wchar_t CLASS_NAME[] = L'sample window class';
+    WNDCLASS wc = { };
+
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+    RegisterClass(&wc);
 }
 
 
@@ -17,7 +34,7 @@ int main()
     HINSTANCE hInstance = GetModuleHandle(NULL);                                                
     std::cout << "Hello World!\n";
 
-    HWND hwnd = CreateWindowEx(WS_EX_TOPMOST | WS_EX_LAYERED, ...);
+    HWND hwnd = CreateWindowExW(WS_EX_TOPMOST | WS_EX_LAYERED, ...);
 
     POINT pt;
     while (true) {
